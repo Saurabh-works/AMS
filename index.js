fc1ba4df184cf45a8fc72269975e5ab4941d53c4
Shivanjali <shivanjalirakhunde2525@gmail.com>
2024-09-19 15:09:51 +0530

    radiant email added

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: "gawaianiket499@gmail.com",
-    pass: "llzp fyoa lmts giwl",
+    user: "lms.radiantitservices@gmail.com",
+    pass: "xtll ytwm ninj ptxp",
   },
 });
 
@@ -82,7 +82,7 @@ app.post("/signup", async (req, resp) => {
 
     // Prepare the email options
     const mailOptions = {
-      from: "gawaianiket499@gmail.com",
+      from: "lms.radiantitservices@gmail.com",
       to: email,
       subject: "Your Attendance Portal Login Details",
       text: `Hello ${name},\n\nWelcome to the Student Attendance Portal. Below are your login credentials:\n\nEmail: ${email}\nPassword: ${password}\nBatch: ${batch}\nStudent ID: ${id}\n\nIt's crucial to keep this information confidential. Do not share your credentials with anyone. They are for your personal use only.\n\nIf you have any questions or concerns, please don't hesitate to reach out.\n\nBest regards,\nAdmin Department,\nRADIANT IT SERVICES PVT. LTD.`,
